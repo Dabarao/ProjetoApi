3f2e487df9f98582729bb747d54db43bb375b16e
Dabarao <danielpontolio15@gmail.com>
2023-09-11 22:09:55 -0300

    primeiro commit

---
+++ src/components/ListPokemons.vue
@@ -0,0 +1,24 @@
+<script setup>
+const pokemon = defineProps(["name", "url"])
+
+
+</script>
+
+<template>
+    <div class="col-4">
+        <div class="card mb-3">
+            <img class="card-img-top" src="https://assets.pokemon.com/assets/cms2/img/pokedex/full/1007.png" alt="Card image cap">
+                <div class="card-body">
+                    <h5 class="card-title">{{ pokemon.name }}</h5>
+                    <a :href="pokemon.url" target="_blank" class="btn btn-primary">Detalhes</a>
+                    
+                </div>
+        </div>
+       
+    </div>
+
+</template>
+
+<style>
+
+</style>
---
+++ src/views/HomeView.vue
@@ -0,0 +1,54 @@
+<script setup>
+import { onMounted, reactive, ref} from 'vue';
+import ListPokemons from '../components/ListPokemons.vue';
+
+let baseUrlSvg = ref("https://raw.githubusercontent.com/PokeApi/sprites/master/sprites/pokemon/other/dream-world/")
+let pokemons = reactive(ref());
+
+onMounted(() => {
+  fetch("https://pokeapi.co/api/v2/pokemon/?limit=20&offset=0")
+  .then(response => response.json())
+  .then(response => {
+    pokemons.value = response.results;
+    console.log(response);
+  })
+})
+
+
+
+</script>
+
+<template>
+  <main>
+    <div class="container">
+      <div class="row mt-4">
+        <div class="col-sm-12 col-md-6">
+          <div class="card" style="width: 18rem;">
+            <img src="https://assets.pokemon.com/assets/cms2/img/pokedex/full/1007.png" class="card-img-top" alt="Imagem Wo-Chien Card">
+            <!-- <img src="https://assets.pokemon.com/assets/cms2/img/pokedex/full/1002.png">            
+            <img src="https://assets.pokemon.com/assets/cms2/img/pokedex/full/1003.png">            
+            <img src="https://assets.pokemon.com/assets/cms2/img/pokedex/full/1004.png"> -->
+            
+            <div class="card-body">
+              <h5 class="card-title">Card title</h5>
+              <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+            </div>
+          </div>
+        </div>
+        <div class="col-sm-12 col-md-6">
+          <div class="card">
+            <div class="card-body row">
+              <ListPokemons 
+                v-for="pokemon in pokemons"
+                :key="pokemon.name"
+                :name="pokemon.name"
+                :url="pokemon.url"
+                
+              />
+            </div>
+          </div>  
+        </div>
+      </div>
+    </div>
+  </main>
+</template>
